be7c63a985cd8c4f1d65bf1d0bbf7de306acf230
ross dibartolo <dibartoloross@gmail.com>
2022-12-23 11:21:35 -0500

    inital commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Corbin";
+            int dogAge = 13;
+            char firstInitial = 'C';
+            bool lovesBones = true;
+            double dogWeight = 75.5;
+            decimal dogFood = 2.5;
+
+            ("$ My dogs name is {dogName} and he is {dogAge} years old. He weighs {dogWeight} and has {dogFood} cups of food twice a day. {dogName} {lovesBones} and gets a new one everytime at the store!")
+            )
         }
     }
 }
